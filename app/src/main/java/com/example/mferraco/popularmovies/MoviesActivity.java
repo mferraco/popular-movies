3f34193217cf698ba4aac716ea845a5ec2289d81
Michael Ferraco <ferraco.mike@gmail.com>
2016-04-02 18:19:26 -0400

    Setting value determines sort order of movies

--- app/src/main/java/com/example/mferraco/popularmovies/MoviesActivity.java
+++ app/src/main/java/com/example/mferraco/popularmovies/MoviesActivity.java
@@ -1,7 +1,11 @@
 package com.example.mferraco.popularmovies;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -18,14 +22,50 @@ public class MoviesActivity extends AppCompatActivity implements AsyncGetMoviesR
 
     private static final String TAG = MoviesActivity.class.getSimpleName();
 
+    private static final String CURRENT_SORT_ORDER_KEY = "mCurrentSortOrder";
+    private static final String FIRST_REQUEST_KEY = "mFirstRequest";
+
+    private GridView mImageGridView;
+
+    private String mCurrentSortOrder;
+
+    private boolean mFirstRequest = true;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movies);
 
-        GetMoviesTask getMoviesTask = new GetMoviesTask(this);
-        getMoviesTask.delegate = this;
-        getMoviesTask.execute("popular");
+        if (savedInstanceState != null) {
+            mCurrentSortOrder = savedInstanceState.getString(CURRENT_SORT_ORDER_KEY, getString(R.string.settings_sort_order_default));
+        } else {
+            mCurrentSortOrder = getString(R.string.settings_sort_order_default);
+        }
+
+        mImageGridView = (GridView) findViewById(R.id.movie_grid_view);
+
+        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            mImageGridView.setNumColumns(4);
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(this);
+        String sortOrderPreference = sharedPref.getString(getString(R.string.settings_sort_order_key), getString(R.string.settings_sort_order_default));
+
+        if (!(mCurrentSortOrder.equalsIgnoreCase(sortOrderPreference)) || mFirstRequest) {
+            // always make the request the first time the activity loads
+            // after that only make the request if the sort order changes
+            mFirstRequest = false;
+            mCurrentSortOrder = sortOrderPreference;
+
+            GetMoviesTask getMoviesTask = new GetMoviesTask(this);
+            getMoviesTask.delegate = this;
+            getMoviesTask.execute(mCurrentSortOrder);
+        }
     }
 
     @Override
@@ -54,7 +94,11 @@ public class MoviesActivity extends AppCompatActivity implements AsyncGetMoviesR
     @Override
     public void processFinish(ArrayList<Movie> movies) {
         // set up image adapter here
-        GridView imageGridView = (GridView) findViewById(R.id.movie_grid_view);
-        imageGridView.setAdapter(new ImageAdapter(this, movies));
+        mImageGridView.setAdapter(new ImageAdapter(this, movies));
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState, PersistableBundle outPersistentState) {
+        outState.putSerializable(CURRENT_SORT_ORDER_KEY, mCurrentSortOrder);
     }
 }
